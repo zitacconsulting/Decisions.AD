562903d52d6382fe1c227f041e1c1f0c012640ca
Kenny Vigersand <kenny.vigersand@zitac.se>
2022-03-23 08:50:37 +0100

    Added Create User

--- Zitac.AD.Steps/CreateUser.cs
+++ Zitac.AD.Steps/CreateUser.cs
@@ -173,6 +173,7 @@ namespace Zitac.AD.Steps
                 {
                     "Error Message",
                     (object) "True"
+                
                 }
                 });
                 }
