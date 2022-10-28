4cb9cee7e2a90f015048c7577d43fda5494c27a0
Kenny Vigersand <kenny.vigersand@zitac.se>
2022-10-28 08:43:15 +0200

    Added server to Get Primary Group function

--- Zitac.AD.Steps/GetUserGroupMemberships.cs
+++ Zitac.AD.Steps/GetUserGroupMemberships.cs
@@ -142,7 +142,7 @@ namespace Zitac.AD.Steps
                 List<Group> GroupList = new List<Group>();
 
 
-                string PrimaryGroupDn = GetUserPrimaryGroup((DirectoryEntry) one.GetDirectoryEntry());
+                string PrimaryGroupDn = GetUserPrimaryGroup((DirectoryEntry) one.GetDirectoryEntry(), ADServer);
                         directorySearcher.Filter = "(&(objectClass=group)(objectCategory=group)(distinguishedname=" + PrimaryGroupDn + "))";
                         SearchResult FoundPrimaryGroup = directorySearcher.FindOne();
                         Group primarygroup = new Group(FoundPrimaryGroup, AdditionalAttributes);
@@ -186,7 +186,7 @@ namespace Zitac.AD.Steps
             }
         }
 
-    private static string GetUserPrimaryGroup(DirectoryEntry de) {
+    private static string GetUserPrimaryGroup(DirectoryEntry de, string ADServer) {
     de.RefreshCache(new[] {"primaryGroupID", "objectSid"});
 
     //Get the user's SID as a string
@@ -198,7 +198,7 @@ namespace Zitac.AD.Steps
     sid = sid + de.Properties["primaryGroupId"].Value;
 
     //Find the group by its SID
-    var group = new DirectoryEntry($"LDAP://<SID={sid}>");
+    var group = new DirectoryEntry($"LDAP://"+ ADServer + "/<SID={sid}>");
     group.RefreshCache(new [] {"distinguishedname"});
 
     return group.Properties["distinguishedname"].Value as string;
